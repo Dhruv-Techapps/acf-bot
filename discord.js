fa6bff30721aec117e13ffd92ea825a349824d64
Dharmesh <dharmesh.hemaram@gmail.com>
2022-12-23 13:16:17 +0530

    Added azure deploy

--- discord.js
+++ discord.js
@@ -1,34 +1,33 @@
 /* eslint-disable no-console */
 const express = require('express')
-const { Client, Intents, MessageEmbed } = require('discord.js')
-require('dotenv').config()
+const { Client, GatewayIntentBits, EmbedBuilder } = require('discord.js')
+
+if (process.env.NODE_ENV !== 'production') {
+  // eslint-disable-next-line global-require
+  require('dotenv').config()
+}
 
 const app = express()
 
-const client = new Client({ intents: [Intents.FLAGS.GUILD_MEMBERS] })
+const client = new Client({ intents: [GatewayIntentBits.GuildMembers] })
 client.on('ready', () => {
   console.log('Bot is online')
 })
-client.login(process.env.BOT_TOKEN)
+client.login(process.env.TOKEN)
 
 function getEmbed(title, fields, variant = 'stable', color = '198754') {
-  const messageEmbed = new MessageEmbed({ title: 'test' })
-  try {
-    messageEmbed
-      .setColor(color.replace('#', ''))
-      .setTitle(title)
-      .setURL(`https://${variant.toLowerCase()}.getautoclicker.com/`)
-      .setThumbnail(`https://blog.getautoclicker.com/icons/${variant.toLowerCase()}_icon48.png`)
-      .setTimestamp()
-      .setFooter(`Auto Clicker - AutoFill ${variant}`, 'https://blog.getautoclicker.com/icons/stable_icon48.png')
-
-    const fieldArray = JSON.parse(fields)
-    fieldArray.forEach(field => {
-      messageEmbed.addFields(field)
-    })
-  } catch (error) {
-    console.error(error)
-  }
+  const messageEmbed = new EmbedBuilder()
+  messageEmbed
+    .setColor(color.replace('#', ''))
+    .setTitle(title)
+    .setURL(`https://${variant.toLowerCase()}.getautoclicker.com/`)
+    .setThumbnail(`https://blog.getautoclicker.com/icons/${variant.toLowerCase()}_icon48.png`)
+    .setTimestamp()
+    .setFooter({ text: `Auto Clicker - AutoFill ${variant}`, iconURL: 'https://blog.getautoclicker.com/icons/stable_icon48.png' })
+  const fieldArray = JSON.parse(fields)
+  fieldArray.forEach(field => {
+    messageEmbed.addFields(field)
+  })
   return messageEmbed
 }
 
@@ -49,4 +48,4 @@ app.get('/notifyDiscord', async ({ query: { id, title, fields, variant = 'stable
   }
 })
 app.get('/', (req, res) => res.send('Hello World'))
-app.listen(process.env.BOT_PORT)
+app.listen(3000)
